9811d3d99da499cab0ac2257c32da5cc68254aa7
albertoacc12-web <alberto.acc12@gmail.com>
2026-05-31 07:00:00 +0200

    Create page.js

---
+++ app/maquinas/page.js
@@ -0,0 +1,19 @@
+export default function Maquinas() {
+  return (
+    <main style={{padding:"20px"}}>
+      <h1>Máquinas</h1>
+
+      <button>
+        ➕ Nueva Máquina
+      </button>
+
+      <p>DIRINLER 1</p>
+
+      <br />
+
+      <a href="/">
+        ← Volver al Dashboard
+      </a>
+    </main>
+  );
+}
